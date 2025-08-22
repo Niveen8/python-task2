889af2c98585924bedb1a97e6d03073d50c20f83
Niveen <niveenabutamam@gmail.com>
2025-08-22 22:09:35 +0300

    enter fruit and cheake

--- task.py
+++ task.py
@@ -3,3 +3,9 @@ print("The fruit:" + ", ".join(fruit))
 print("the first:",fruit[0])
 print("the last:",fruit[-1])
 fruit[1] = "Mango"
+fruit.insert(2, "Watermelon")
+enter_fruit = input("Enter a fruit name: ")
+if enter_fruit  in fruit:
+    print("Yes available")
+else:
+    print("No available")
