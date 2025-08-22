dd24ad80e79bb6f7fe82416e6f9d10c0284f68f7
Niveen <niveenabutamam@gmail.com>
2025-08-22 21:01:02 +0300

    add first,last and join

--- task.py
+++ task.py
@@ -1,2 +1,4 @@
 fruit = ["Grapes", "Mango", "Berry", "Watermelon", "Peach"]
-print("The fruit:" +fruit)
+print("The fruit:" + ", ".join(fruit))
+print("the first:",fruit[0])
+print("the last:",fruit[-1])
